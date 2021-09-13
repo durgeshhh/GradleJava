03443ac605eb32e603d5b6361bba59efaea82c47
durgesh tiwari <durgeshhh@gmail.com>
2021-09-13 01:10:59 +0100

    gradle java intial code

---
+++ app/src/test/java/GradelDemo/AppTest.java
@@ -0,0 +1,15 @@
+package GradleDemo;
+
+import org.testng.annotations.*;
+import static org.testng.Assert.*;
+
+public class AppTest{
+   @Test public void appHasAGreeting()
+    {
+      //Arrange
+      App myApp=new App();
+      //Act and Assert
+      assertNotNull(myApp.getGreeting(), "app should have a greeting");
+    } 
+
+}
